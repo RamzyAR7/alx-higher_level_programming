9c3ad9dbba8cea6045d3015b46ce23e2842be5fd
RamzyAR7 <rameya683@gmail.com>
2024-01-03 00:02:41 +0200

    add v3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char in map(str, range(97, 123)):
-    if char == 'q' and char == 'e':
+for char in range(97, 123):
+    if char == "q" and char == "e":
         continue
-    print("{}".format(char), end="")
+    print("{}".format(chr(char)), end="")
