f02f6476de02961504730d89cc3ca126d75973de
RamzyAR7 <rameya683@gmail.com>
2024-01-05 01:07:38 +0200

    add v2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-argc = len(argv)
-if argv == 1:
-    print("0 arguments.")
-elif argv == 2:
-    print("1 argument:")
-    print("1:{}".format(argv[1]))
-else:
-    print("{} arguments:".format(len(argv)))
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+        print("1:{}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv)))
 
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{:d}: {}".format(i, arg))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{:d}: {}".format(i, arg))
