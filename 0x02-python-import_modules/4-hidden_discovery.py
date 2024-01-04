2dc09bd73ef84602532ecfc463c19023bf82a238
RamzyAR7 <rameya683@gmail.com>
2024-01-05 01:46:39 +0200

    add v4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    names_of_func = dir(hidden_4)
+    for n in names_of_func:
+        if n[:2] == "_":
+            continue
+        else:
+            print(n);
