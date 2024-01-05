81fba59995f1f43a3a3f5afd338b1f588101a440
RamzyAR7 <rameya683@gmail.com>
2024-01-05 03:24:59 +0200

    add v100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,28 +4,27 @@ def calc():
     from calculator_1 import add, sub, mul, div
     argc = len(argv) - 1
 
-    if argc == 3:
-
-        if argv[2] not in {'+', '-', '*', '/'}:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-
-        a = int(argv[1])
-        b = int(argv[3])
-        operator = argv[2]
-        result = 0
-
-        if operator == '+':
-            result = add(a, b)
-        elif operator == '-':
-            result = sub(a, b)
-        elif operator == '/':
-            result = div(a, b)
-        elif operator == '*':
-            result = mul(a, b)
-    else:
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    if argv[2] not in {'+', '-', '*', '/'}:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    result = 0
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    elif operator == '*':
+        result = mul(a, b)
 
     print("{} {} {} = {}".format(a, operator, b, result))
 
