8b013706e0934db1015bc769d86b5063cf500cc1
RamzyAR7 <rameya683@gmail.com>
2024-01-07 04:26:53 +0200

    add v8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None
+        return 0, None
     length = len(sentence)
     first_char = sentence[0]
     return length, first_char
