2a78e5b78c2e4713dc2033a3d348e20461abd2ea
RamzyAR7 <rameya683@gmail.com>
2024-01-07 04:41:41 +0200

    add v9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_num = my_list[0]
+    list_len = len(my_list)
+
+    for i in range(0, list_len):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return max_num
