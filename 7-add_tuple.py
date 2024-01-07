67413322bbf4c29f86bce2aa1fd33c6e56e67476
RamzyAR7 <rameya683@gmail.com>
2024-01-07 04:03:36 +0200

    add v7

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -8,5 +8,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b + (0, 0)
     elif len(tuple_b) < 2:
         tuple_b = tuple_b + (0,)
-    tuple_n = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return tuple_n
+
+    return  (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
