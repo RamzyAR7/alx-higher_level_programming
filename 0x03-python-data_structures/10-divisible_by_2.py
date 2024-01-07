557b4cf043d60fbcead64c05139151f1c0554acf
RamzyAR7 <rameya683@gmail.com>
2024-01-07 05:00:13 +0200

    add v10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     list_str = []
     for n in my_list:
         if n % 2 == 0:
-            list_str += ["true",]
+            list_str += ["True",]
         else:
-            list_str += ["false",]
+            list_str += ["False",]
     return list_str
