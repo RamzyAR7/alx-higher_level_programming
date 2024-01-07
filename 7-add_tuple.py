8bc75945b5cbee70ed8d4a17f7285eec30c4fde3
RamzyAR7 <rameya683@gmail.com>
2024-01-07 03:56:27 +0200

    add v7

--- 7-add_tuple.py
+++ 7-add_tuple.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0:
-        tuple_a = (0, 0)
-    if len(tuple_b) == 0:
-        tuple_b = (0, 0)
-    if len(tuple_a)  == 1:
-        tuple_a += (0,)
-    if len(tuple_b) == 1:
-        tuple_b += (0,)
-
-    n1 = tuple_a[0] + tuple_b[0]
-    n2 = tuple_a[1] + tuple_b[1]
-
-    tuple_n = (n1, n2)
-    return tuple_n
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0, 0)
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0, 0)
+    elif len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return tuple_c
