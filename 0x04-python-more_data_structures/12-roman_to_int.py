b5761dc1d038f1603f001e28a3a94d3e824f930f
RamzyAR7 <rameya683@gmail.com>
2024-01-11 01:27:10 +0200

    add v12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    sum = 0
+    prev = 0
+    romn_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for k in reversed(roman_string):
+        current = romn_num[k]
+        if prev <= current:
+            sum += current
+        else:
+            sum -= current
+        prev = current
+
+    return sum
