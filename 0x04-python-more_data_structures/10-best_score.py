b02eb1eff0c7d106184499955a691b51dc050b6b
RamzyAR7 <rameya683@gmail.com>
2024-01-10 23:26:20 +0200

    add v10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -14,11 +14,3 @@ def best_score(a_dictionary):
         return best_k
     else:
         return None
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
