d3b1deec9e16cdbab093f7e6c3c8e7a508bba47a
RamzyAR7 <rameya683@gmail.com>
2024-01-11 02:22:40 +0200

    add v102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary.items():
-        if v == value:
-            a_dictionary.pop(k)
+    key_to_remove = [k for k, v in a_dictionary.items() if v == value]
+
+    if key_to_remove == []:
+        return a_dictionary
+
+    for k in key_to_remove:
+        a_dictionary.pop(k)
     return a_dictionary
