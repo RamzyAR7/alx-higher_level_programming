71ab4a8ce0d3fd895c16f5646e9aa5544478be9e
RamzyAR7 <rameya683@gmail.com>
2024-01-11 02:47:42 +0200

    add v103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,111 +1,70 @@
-#include <Python.h>
 #include <stdio.h>
+#include <Python.h>
+
 
 /**
- * print_python_bytes - Print information about Python bytes objects
- * @p: PyObject pointer
+ * write_python - Writes Python object information.
+ *
+ * @python: Python object to write information about.
  */
-void print_python_bytes(PyObject *p)
+void write_python(PyObject *python)
 {
-	PyBytesObject *bytes;
-	unsigned long size, i;
+	char *str;
+	long int s, i, lmt;
 
 	printf("[.] bytes object info\n");
-
-	if (!PyBytes_Check(p))
+	if (!PyBytes_Check(python))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	bytes = (PyBytesObject *)p;
-	size = PyBytes_Size(p);
+	s = ((PyVarObject *)(python))->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
 
-	printf("  size: %lu\n", size);
-	printf("  trying string: %s\n", bytes->ob_sval);
+	printf("  size: %ld\n", s);
+	printf("  trying string: %s\n", str);
 
-	printf("  first 10 bytes: ");
-	for (i = 0; i < size && i < 10; ++i)
-	{
-		printf("%02x ", (unsigned char)bytes->ob_sval[i]);
-	}
+	if (s >= 10)
+		lmt = 10;
+	else
+		lmt = s + 1;
+
+	printf("  first %ld bytes:", lmt);
+
+	for (i = 0; i < lmt; i++)
+		if (str[i] >= 0)
+			printf(" %02x", str[i]);
+		else
+			printf(" %02x", 256 + str[i]);
+
 	printf("\n");
 }
 
+
 /**
- * print_python_list - Print information about Python lists
- * @p: PyObject pointer
+ * write_python_l - Writes information about a Python object.
+ *
+ * @python: Python object to write information about.
  */
-void print_python_list(PyObject *p)
+void write_python_l(PyObject *python)
 {
+	long int s, i;
 	PyListObject *list;
-	Py_ssize_t size, i;
+	PyObject *obj;
+
+	s = ((PyVarObject *)(python))->ob_size;
+	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
-
-	if (!PyList_Check(p))
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
-	list = (PyListObject *)p;
-	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Size of the Python List = %ld\n", s);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < size; ++i)
+	for (i = 0; i < s; i++)
 	{
-		PyObject *element = PyList_GetItem(p, i);
-
-		printf("Element %ld: ", i);
-		if (PyBytes_Check(element))
-			print_python_bytes(element);
-
-		else if (PyLong_Check(element))
-			printf("int\n");
-
-		else if (PyFloat_Check(element))
-			printf("float\n");
-
-		else if (PyTuple_Check(element))
-			printf("tuple\n");
-
-		else if (PyList_Check(element))
-			printf("list\n");
-
-		else if (PyUnicode_Check(element))
-			printf("str\n");
-		else
-			printf("[ERROR] Unsupported Element Type\n");
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			write_python(obj);
 	}
-}
-
-/**
- * main - Example usage
- * Return: Always 0
- */
-int main(void)
-{
-	Py_Initialize();
-
-	PyObject *s = PyBytes_FromString("Hello");
-
-	print_python_bytes(s);
-
-	PyObject *b = PyBytes_FromStringAndSize("\xff
-										\xf8\x00\x00\x00\x00\x00\x00", 8);
-	print_python_bytes(b);
-
-	PyObject *l = PyList_New(2);
-
-	PyList_SetItem(l, 0, s);
-	PyList_SetItem(l, 1, PyBytes_FromString("World"));
-	print_python_list(l);
-
-	Py_DECREF(l);
-	Py_DECREF(s);
-	Py_DECREF(b);
-
-	Py_Finalize();
-	return (0);
 }
