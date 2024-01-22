84e291fb5df2038b2a4ced5d14193fba1705b1a1
RamzyAR7 <rameya683@gmail.com>
2024-01-22 12:01:20 +0200

    fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             print()
             print("list index out of range")
             break
-
     print()
     return z
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,14 +10,3 @@ def safe_print_division(a, b):
         return z
     finally:
         print("Inside result: {:f}".format(z))
-
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
