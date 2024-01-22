f412812e9cf27997d2c39a351f1321999dcf73c0
RamzyAR7 <rameya683@gmail.com>
2024-01-22 11:54:34 +0200

    add v5

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for idx in range(0, x):
             if isinstance(my_list[idx], int):
-                print("{}".format(my_list[idx]))
+                print("{:d}".format(my_list[idx]))
                 z += 1
         print()
 
