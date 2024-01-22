42cbb89e439c5acd7154b6f1414c340057561586
RamzyAR7 <rameya683@gmail.com>
2024-01-22 11:53:24 +0200

    add v5

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     z = 0
     try:
         for idx in range(0, x):
-            if type(my_list[idx]) is int:
+            if isinstance(my_list[idx], int):
                 print("{}".format(my_list[idx]))
                 z += 1
         print()
