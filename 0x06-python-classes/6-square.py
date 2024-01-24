8bf485c531e8b34f969e7d093ae873e8ea0e9ba7
RamzyAR7 <rameya683@gmail.com>
2024-01-24 14:45:43 +0200

    add v6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,11 +47,12 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        if self.__size > 0:
+        if self.__size == 0:
+            print()
+        else:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
-        else:
-            print()
+                for _ in range(self.__position[0]):
+                    print(" ",  end="")
+                print("#" * (self.__size))
