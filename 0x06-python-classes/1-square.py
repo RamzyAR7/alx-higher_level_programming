9c6e61574cfb945cee05d589904a4ab502938d0e
RamzyAR7 <rameya683@gmail.com>
2024-01-24 12:37:22 +0200

    add v1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module for defines a class Square"""
+
+
+class Square:
+    """This class is a squeleton for a square"""
+    def __init__(self, size):
+        self.__size = size
