89080c882500c451514f2cef85da7da3be1ad6d6
RamzyAR7 <rameya683@gmail.com>
2024-02-06 01:41:01 +0200

    add 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""this module for class Rectangle"""
+
+
+from _7-base_geometry.py import BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        """init methoud"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
