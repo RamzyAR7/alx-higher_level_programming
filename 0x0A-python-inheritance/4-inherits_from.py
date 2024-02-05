36c3f3a462ff84dd7e8741f015f9bf65f9d53456
RamzyAR7 <rameya683@gmail.com>
2024-02-06 01:11:00 +0200

    add 7

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     function
     """
 
-    return not type(obj) == a_class and isinstance((obj), a_class)
+    return type(obj) != a_class and isinstance((obj), a_class)
