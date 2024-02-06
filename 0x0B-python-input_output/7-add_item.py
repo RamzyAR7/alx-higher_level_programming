eec84354cd01b5565bd1c96d45c801affe280417
RamzyAR7 <rameya683@gmail.com>
2024-02-07 01:59:37 +0200

    add v7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,17 +6,16 @@ to a Python list, and then save them to a file
 
 
 import sys
-from _5-save_to_json_file import save_to_json_file
-from _6-load_from_json_file import load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
-def main():
-    argv = sys.argv[1:]
+argv = sys.argv[1:]
 
-    try:
-        json_str = load_from_json_file("add_item.json")
-    except FileExistsError:
-        json_str = []
-    
-    json_str.extend(argv)
-    save_to_json_file(json_str, "add_item.json")
+try:
+    json_str = load_from_json_file("add_item.json")
+except FileExistsError:
+    json_str = []
+
+json_str.extend(argv)
+save_to_json_file(json_str, "add_item.json")
