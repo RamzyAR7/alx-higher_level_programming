22c8c2fa3c8a7c304dc1bca66226a4f9cadeb156
RamzyAR7 <rameya683@gmail.com>
2024-02-06 02:02:07 +0200

    add 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+    def area(self):
+        """rectangle area"""
+        return self.__width*self.__height
+
     def __str__(self):
         """str func"""
         return f"[Rectangle] {self.__width} / {self.__height}"
-
-    def area(self):
-        """rectangle area"""
-        return self.__width * self.__height
