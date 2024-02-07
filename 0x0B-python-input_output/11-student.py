2d8ec8cf8af520041894b507adf2dab76b66f21c
RamzyAR7 <rameya683@gmail.com>
2024-02-07 03:04:41 +0200

    add v11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+this module for Student class
+"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return dict of all attribute as key & value"""
+        js_dict = {}
+        for key, value in self.__dict__.items():
+            if not isinstance(attrs, list) or key in attrs:
+                js_dict[key] = value
+        return js_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student"""
+        for key, value in json.items():
+            self.__dict__[key] = value
