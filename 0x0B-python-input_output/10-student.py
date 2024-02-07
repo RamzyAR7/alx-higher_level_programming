c136fb8e3d3f6fe92a72408620be39e2c33ddd1e
RamzyAR7 <rameya683@gmail.com>
2024-02-07 02:48:18 +0200

    add v9

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this module for Student class
+"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return dict of all attribute as key & value"""
+        if attrs is None or not attrs:
+            return self.__dict__
+
+        js_dict = {}
+        for x in attrs:
+            if hasattr(self, x):
+                js_dict[x] = getattr(self, x)
+        return js_dict
