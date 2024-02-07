c54ed42a2a6b1681f19152f2f67dbbeb23567cc8
RamzyAR7 <rameya683@gmail.com>
2024-02-07 03:25:46 +0200

    add v101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
 """this module for stats"""
-import sys
+def print_metrics(size, status_codes):
+    """
+    Print accumulated metrics
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
-def print_metrics(total_size, status_codes):
-    """Prints the computed metrics."""
-    print("Total file size: {}".format(total_size))
-    for code, count in sorted(status_codes.items()):
-        print("{}: {}".format(code, count))
+if __name__ == "__main__":
+    import sys
 
-
-def main():
-    total_size = 0
-    status_counts = {}
+    size = 0
+    status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    line_count = 0
+    count = 0
 
     try:
         for line in sys.stdin:
-            if line_count == 10:
-                print_metrics(total_size, status_counts)
-                line_count = 1
+            if count == 10:
+                print_metrics(size, status_codes)
+                count = 1
             else:
-                line_count += 1
+                count += 1
 
-            parts = line.split()
+            line = line.split()
 
             try:
-                total_size += int(parts[-1])
+                size += int(line[-1])
             except (IndexError, ValueError):
                 pass
 
             try:
-                if parts[-2] in valid_codes:
-                    status_counts[parts[-2]] = status_counts.get(parts[-2], 0) + 1
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
-        print_metrics(total_size, status_counts)
+        print_metrics(size, status_codes)
 
     except KeyboardInterrupt:
-        print_metrics(total_size, status_counts)
+        print_metrics(size, status_codes)
         raise
-
-
-if __name__ == "__main__":
-    main()
