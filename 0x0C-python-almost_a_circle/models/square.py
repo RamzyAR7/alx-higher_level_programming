29f91df38c94d094b035d24b165adc84d99881cf
RamzyAR7 <rameya683@gmail.com>
2024-02-13 05:05:33 +0200

    add

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initalization"""
         super().__init__(size, size, x, y, id)
@@ -40,5 +41,9 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        return {"id": self.id, "size": self.size,
-                "x": self.x, "y": self.y}
+        """get dictionary copy of the class"""
+        class_dict = {}
+        keys = ("id", "size", "x", "y")
+        for key in keys:
+            class_dict[key] = getattr(self, key)
+        return class_dict
