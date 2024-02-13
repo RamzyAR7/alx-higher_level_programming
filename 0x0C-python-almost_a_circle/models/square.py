b0137e83a348b0f388a6b889b407d33eed7b68da
RamzyAR7 <rameya683@gmail.com>
2024-02-13 04:44:26 +0200

    add

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,26 +5,26 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    '''A Square class.'''
+
     def __init__(self, size, x=0, y=0, id=None):
-        """Initalization"""
+        '''Constructor.'''
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter method"""
+        '''Size of this square.'''
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter method"""
         self.width = value
         self.height = value
-
-    def __str__(self):
-        '''Returns string info about this square.'''
-        return f"[{type(self).__name__}] ({self.id}) \
-{self.x}/{self.y} - {self.width}"
 
     def update(self, *args, **kwargs):
         """update if you use args"""
