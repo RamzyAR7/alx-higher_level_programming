fc247d23a5125b88308a8ecdc2d532f9d30d3fe4
RamzyAR7 <rameya683@gmail.com>
2024-03-14 04:26:06 +0200

    add v6,7

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE table IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, name VARCHAR(256) NOT NULL);
