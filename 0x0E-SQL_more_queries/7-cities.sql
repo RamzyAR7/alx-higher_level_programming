e751c1af1c6948a5a304e0bc7ccf341e512c23fe
RamzyAR7 <rameya683@gmail.com>
2024-03-14 04:28:27 +0200

    add v6,7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---a script that creates the database hbtn_0d_usa and the table cities
+-- creates the database hbtn_0d_usa and the table cities
 CREATE database IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS cities(
