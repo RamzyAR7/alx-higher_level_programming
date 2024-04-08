0b20b7b07333526c682b2acd90d434002b2443f6
RamzyAR7 <rameya683@gmail.com>
2024-04-09 00:29:45 +0200

    add v10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 function Factorial (x) {
-	if (x === 0) {
-		return (1);
-	}
-	return (Factorial(x - 1) * x);
+  if (x === 0) {
+    return (1);
+  }
+  return (Factorial(x - 1) * x);
 }
 const arg = process.argv[2];
 let res;
 
 if (!arg || isNaN(arg) || arg === 1) {
-	res = 1;
-}
-else {
-	res = Factorial(parseInt(arg));
+  res = 1;
+} else {
+  res = Factorial(parseInt(arg));
 }
 console.log(res);
