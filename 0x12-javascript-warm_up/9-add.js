85d8951c3e113eaf2261503b78e481cf2728f5d3
RamzyAR7 <rameya683@gmail.com>
2024-04-09 00:09:06 +0200

    add v9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  console.log(a + b);
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!arg1 || !arg2) {
+  console.log('NaN');
+} else {
+  add(arg1, arg2);
+}
