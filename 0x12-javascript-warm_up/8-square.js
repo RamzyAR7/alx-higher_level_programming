474990277c9883c13294584dcc195fe5424e3b57
RamzyAR7 <rameya683@gmail.com>
2024-04-08 23:47:27 +0200

    add v8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 
-let count = '';
 let i, x;
 for (i = 1; i <= arg; i++) {
+  let count = '';
   for (x = 0; x < arg; x++) {
     count += 'x';
   }
-  if (i !== arg) {
-    count += '\n';
-  }
+  console.log(count);
 }
-console.log(count);
