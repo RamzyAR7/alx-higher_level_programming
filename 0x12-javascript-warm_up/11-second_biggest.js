de0bb195c11e4a44ea8bfcc7ed1fcb7166a41b12
RamzyAR7 <rameya683@gmail.com>
2024-04-09 01:01:49 +0200

    add v11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const arg = process.argv.slice(2).map(Number);
+
+if (arg.length < 2) {
+  console.log(0);
+} else {
+  let max = arg[0];
+  let max2 = arg[1];
+
+  for (let i = 1; i < arg.length; i++) {
+    if (arg[i] > max) {
+      max2 = max;
+      max = arg[i];
+    } else if (arg[i] > max2 && arg[i] < max) {
+      max2 = arg[i];
+    }
+  }
+
+  console.log(max2);
+}
