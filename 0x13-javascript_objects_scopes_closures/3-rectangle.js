47626a9b3bc2107611eb019a2ed2684c20620d86
RamzyAR7 <rameya683@gmail.com>
2024-04-09 23:43:10 +0200

    add v3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let x, y;
+    for (x = 0; x < this.height; x++) {
+        let rec = '';
+        for (y = 0; y < this.width; y++) {
+            rec += 'X';
+        }
+        console.log(rec);
+    }
+  }
+}
+
+module.exports = Rectangle;
