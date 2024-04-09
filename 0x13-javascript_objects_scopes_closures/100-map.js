a9518d154650d72b71c30220323cd0a749712c78
RamzyAR7 <rameya683@gmail.com>
2024-04-10 01:09:31 +0200

    add v10

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const lists = require('./100-data.js').list;
-
-const newList = lists.map((value, index) => value * index);
-
+const lists = require('./100-data').list;
 console.log(lists);
+const newList = lists.map((items, index) => items * index);
 console.log(newList);
