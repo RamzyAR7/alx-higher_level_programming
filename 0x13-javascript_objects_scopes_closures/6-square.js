dd5f73b29e64773f278395355e0b2bbc2fe904fa
RamzyAR7 <rameya683@gmail.com>
2024-04-10 00:05:08 +0200

    add v6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (c) {
+        let x, y;
+        for (x = 0; x < this.height; x++) {
+        let rec = '';
+        for (y = 0; y < this.width; y++) {
+            rec += c;
+        }
+        console.log(rec);
+        }
+    } else {
+        super.print();
+    }
+  }
+}
+
+module.exports = Square;
