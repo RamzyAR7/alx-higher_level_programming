34f38c107e7a973e0cc9b1ab8187bf1ca107f1ba
RamzyAR7 <rameya683@gmail.com>
2024-04-09 23:31:54 +0200

    add v2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,11 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-	  this.width = w;
+      this.width = w;
       this.height = h;
     } else {
-      this.width;
-      this.height;
+      this.width = undefined;
+      this.height = undefined;
     }
   }
 }
