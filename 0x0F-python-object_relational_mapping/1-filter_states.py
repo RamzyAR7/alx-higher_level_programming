3b3b94435c05e55426579f3ce7f51d01e9787174
RamzyAR7 <rameya683@gmail.com>
2024-04-15 02:44:52 +0200

    add v1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+this model for  lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    """ main funcion """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("""SELECT *
+                FROM states
+                WHERE name LIKE 'N%'
+                ORDER BY id ASC
+                """)
+    res = cur.fetchall()
+    for row in res:
+        print(row)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
