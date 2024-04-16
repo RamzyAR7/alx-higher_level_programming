dbdaea63a5fc102aa712de38ab70219faf02877b
RamzyAR7 <rameya683@gmail.com>
2024-04-16 04:23:58 +0200

    add v103

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+get data from database (CRD OP)
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+    data = session.query(State).order_by(State.id)
+    for value in data:
+        for city in value.cities:
+            print(f'{city.id}: {city.name} -> {value.name}')
