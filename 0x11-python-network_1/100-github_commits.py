24c186472951533bd54c1c7802c8f9fae59fb69c
RamzyAR7 <rameya683@gmail.com>
2024-04-30 17:40:34 +0300

    add v8

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+from sys import argv
+import requests
+
+
+def main():
+    val = "" if len(argv) == 1 else argv[1]
+    values = {"q": val}
+    with requests.post("http://0.0.0.0:5000/search_user", data=values) as res:
+        try:
+            output = res.json()
+            if output:
+                print("[{}] {}".format(output.get("id"), output.get("name")))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
