47bada7a20c71f355606769752a1b402adf0e6bf
RamzyAR7 <rameya683@gmail.com>
2024-04-30 17:32:34 +0300

    add v6

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    
+    with requests.get(argv[1]) as req:
+        if req.ok:
+            print(req.text)
+        else:
+            print(f"Error code: {req.status_code}")
