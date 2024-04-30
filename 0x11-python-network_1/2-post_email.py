7b67799c25fc0fbd6ce82fec7532644351a5538d
RamzyAR7 <rameya683@gmail.com>
2024-04-30 16:14:14 +0300

    add v2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,18 +7,18 @@ from urllib.request import Request, urlopen
 def main():
     # Extracting URL and email from command-line arguments
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {'email': sys.argv[2]}
 
     # Encoding the email parameter
-    data = urlencode({'email': email}).encode('ascii')
-
+    data = urlencode(value)
+    data = data.encode("ascii")
     # Creating a POST request
-    req = Request(url, data=data, method='POST')
+    req = Request(url, data)
 
     # Sending the request and retrieving the response
     with urlopen(req) as response:
         # Reading and printing the response
-        print(response.read().decode('utf-8', "replace"))
+        print(response.read().decode('utf-8'))
 
 
 if __name__ == "__main__":
