03765f3546503a0cc2286a138008ccffdefe80e4
RamzyAR7 <rameya683@gmail.com>
2024-04-30 14:53:17 +0300

    add v0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+with request.urlopen("https://alx-intranet.hbtn.io/status") as respone:
+    site = respone.read()
+
+print("Body response:")
+print("\t- type:", type(site))
+print("\t- content:", site)
+print("\t- utf8 content:", site.decode('utf-8'))
