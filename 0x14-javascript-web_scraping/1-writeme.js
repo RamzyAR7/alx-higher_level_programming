986dfd0868e372645706428e0efc7df5d32bdd64
RamzyAR7 <rameya683@gmail.com>
2024-05-21 23:53:30 +0300

    add v1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// a script that writes a string to a file.
+
+const argv = process.argv;
+let file = argv[2]
+let txt = argv[3]
+
+const fs = require('fs')
+fs.writeFile(file, txt, 'utf8', function (err){
+    if(err){
+        console.log(err);
+    }
+});
