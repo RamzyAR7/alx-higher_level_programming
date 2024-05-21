25a505bf6ec9493f6e6506280a2d78c1d68e2f2d
RamzyAR7 <rameya683@gmail.com>
2024-05-22 00:55:04 +0300

    add v101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-// Get the Movie ID from the command line arguments
-const movieId = process.argv[2];
-
-// Define the Star Wars API URL
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Make a GET request to the Star Wars API
-request(apiUrl, function (err, response, body) {
+const endPoint = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(endPoint, function (err, response, body) {
   if (err) {
-    console.error('Error:', err);
-    return;
-  }
-
-  // Parse the JSON response
-  const data = JSON.parse(body);
-
-  // Print the character names
-  data.characters.forEach(characterUrl => {
-    request(characterUrl, function (err, response, body) {
-      if (err) {
-        console.error('Error:', err);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+    throw err;
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(character => {
+      request.get(character, function (err, response, body) {
+        if (err) {
+          throw err;
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     });
-  });
+  }
 });
