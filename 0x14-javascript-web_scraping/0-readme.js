48fa862ece3398775f986b7443978c4ac48be6c7
RamzyAR7 <rameya683@gmail.com>
2024-05-21 23:41:28 +0300

    add v0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-//a script that reads and prints the content of a file.
+// a script that reads and prints the content of a file.
 
 const argv = process.argv;
-let fs = require('fs');
+const fs = require('fs');
 fs.readFile(argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
